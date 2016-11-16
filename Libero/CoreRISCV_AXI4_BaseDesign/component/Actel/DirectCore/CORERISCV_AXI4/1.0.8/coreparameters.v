05429048522fd279acf2e2bcc368d670cd362964
CLappin <ciaran.lappin@microsemi.com>
2016-11-16 17:54:55 +0000

    Added README.md to CoreRISCV_AXI4_BaseDesign Libero project.

--- Libero/CoreRISCV_AXI4_BaseDesign/component/Actel/DirectCore/CORERISCV_AXI4/1.0.8/coreparameters.v
+++
@@ -1,7 +0,0 @@
-//--------------------------------------------------------------------
-// Created by Microsemi SmartDesign Tue Nov 15 12:57:50 2016
-// Parameters for CORERISCV_AXI4
-//--------------------------------------------------------------------
-
-
-parameter RESET_VECTOR_ADDR = 'h60000000;
