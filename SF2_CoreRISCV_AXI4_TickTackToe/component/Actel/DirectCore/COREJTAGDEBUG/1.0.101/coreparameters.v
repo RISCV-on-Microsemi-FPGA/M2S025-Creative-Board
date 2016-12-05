23fe1df1c5b16f79b762cbfc4cd8572edf5d7ff7
CLappin <ciaran.lappin@microsemi.com>
2016-12-05 22:41:02 +0000

    Initial Tick Tack Toe Libero design files

---
+++ SF2_CoreRISCV_AXI4_TickTackToe/component/Actel/DirectCore/COREJTAGDEBUG/1.0.101/coreparameters.v
@@ -0,0 +1,8 @@
+//--------------------------------------------------------------------
+// Created by Microsemi SmartDesign Mon Dec  5 16:40:35 2016
+// Parameters for COREJTAGDEBUG
+//--------------------------------------------------------------------
+
+
+parameter FAMILY = 19;
+parameter IR_CODE = 'h55;
---
+++ SF2_CoreRISCV_AXI4_TickTackToe/component/Actel/DirectCore/CoreTimer/2.0.103/coreparameters.v
@@ -0,0 +1,9 @@
+//--------------------------------------------------------------------
+// Created by Microsemi SmartDesign Mon Dec  5 16:40:35 2016
+// Parameters for CoreTimer
+//--------------------------------------------------------------------
+
+
+parameter FAMILY = 19;
+parameter INTACTIVEH = 1;
+parameter WIDTH = 32;
